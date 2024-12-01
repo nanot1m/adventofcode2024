ea87be780d3e4e5b3ddaf193e5a6fec66d955885
Timur Khazamov <bboytx@gmail.com>
2024-12-01 10:36:42 +0100

    Day 1. JS

--- js/solutions/1.js
+++ js/solutions/1.js
@@ -1,5 +1,7 @@
 // @ts-check
 
+import { transpose } from "../modules/array2d.js"
+import { compareAsc } from "../modules/lib.js"
 import { t } from "../modules/parser.js"
 
 export const useExample = false
@@ -9,18 +11,27 @@ export const exampleInput = `\
 
 /** @typedef {ReturnType<typeof parseInput>} InputType */
 
-export const parseInput = t.arr(t.int()).parse
+export const parseInput = t.arr(t.arr(t.int())).map(transpose).parse
 
 /**
  * @param {InputType} input
  */
 export function part1(input) {
-	return input.values().sum()
+	const [leftArr, rightArr] = input.map((arr) => arr.sort(compareAsc))
+	return leftArr
+		.values()
+		.zip(rightArr)
+		.map(([a, b]) => Math.abs(a - b))
+		.sum()
 }
 
 /**
  * @param {InputType} input
  */
 export function part2(input) {
-	return input.values().sum()
+	const [leftArr, rightArr] = input
+	return leftArr
+		.values()
+		.map((n) => rightArr.values().count((m) => m === n) * n)
+		.sum()
 }
